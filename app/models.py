d555a42fa8c0d27bef5951f735d300d70ecb9200
JATIN-PANDAY <jatinpanday136@gmail.com>
2023-12-27 12:07:13 +0530

    Changes

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from base.models import BaseModel
 
 # Create your models here.
+
+    
+class Regestration(BaseModel):
+    name = models.CharField(max_length=50, null=True)
+    password=models.CharField(max_length=10,null=True)
+    image = models.ImageField(upload_to='student/images/', blank=True, null=True)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from app.models import *
+from django.http import HttpResponseRedirect,HttpResponse
+
+from django.contrib import messages
 
 # Create your views here.
+
+def index(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        password=request.POST.get('password')
+        image = request.FILES.get('image')
+        regestration=Regestration.objects.create(name=name,image=image,password=password)
+        messages.success(request, 'Registration successful!')
+        return HttpResponseRedirect(request.path_info)
+
+    return render(request,"base.html")
